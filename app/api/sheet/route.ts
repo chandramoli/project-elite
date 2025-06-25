d8691768d49b670cf321711fe7fe0d0e672f2b6a
chandramoli <chandanchandrawat7@gmail.com>
2025-06-25 15:24:57 +0530

    google api added for sheets

---
+++ app/api/sheet/route.ts
@@ -0,0 +1,35 @@
+// app/api/sheet/route.ts
+import { google } from 'googleapis';
+import path from 'path';
+import { NextResponse } from 'next/server';
+import { promises as fs } from 'fs';
+
+export async function GET() {
+  try {
+    const keyFilePath = path.join(process.cwd(), 'credentials.json');
+    const keyFile = await fs.readFile(keyFilePath, 'utf8');
+
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth });
+
+    const spreadsheetId = '16qqqxTG-N7DCs2lJQZvzvELIzsJd2UeryeaMyHl3JT8';
+    const range = 'Sheet1';
+
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+
+    const rows = response.data.values;
+    console.log('Fetched rows:', response);
+
+    return NextResponse.json({ data: rows });
+  } catch (error) {
+    console.error('Error fetching sheet data:', error);
+    return NextResponse.json({ error: 'Failed to fetch sheet data' }, { status: 500 });
+  }
+}
