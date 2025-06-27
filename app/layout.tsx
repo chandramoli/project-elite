f5b28eefd994556413252ca0d9549be07dd8bb1e
chandramoli <chandanchandrawat7@gmail.com>
2025-06-27 21:08:36 +0530

    montserra font imported in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Montserrat } from "next/font/google";
 import "./globals.css";
 import LayoutWireFrame from "@/modules/layout";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,6 +14,11 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+})
 
 export default function RootLayout({
   children,
@@ -20,10 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en"  className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}>
+      <body>
         <LayoutWireFrame>
 
         {children}
